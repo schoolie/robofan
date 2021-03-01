890db2ab8a54eda0fe9324522d04f1a59fafb979
Brian Schoolcraft <brian@humanreadable.co>
2021-02-28 22:18:10 -0600

    improve stepper driver, full control over microstep

--- stepper/driver.py
+++ stepper/driver.py
@@ -1,18 +1,58 @@
 import time
 import pigpio
 
+default_pins = dict(
+    enable=6,
+    ms1=5,
+    ms2=25,
+    ms3=24,
+    reset=23,
+    sleep=22,
+    step=17,
+    direction=4,
+)
+
+microstep_truth_table = [  #(ms1, ms2, ms3)
+    (0,0,0), # whole step
+    (1,0,0), # half step
+    (0,1,0), # quarter step
+    (1,1,0), # eigth step
+    (1,1,1), # sixteenth step
+]
+
+power_map = {
+    1: 0,
+    2: 1,
+    4: 2,
+    8: 3,
+    16: 4,
+}
+
 class StepperDriver():
-    def __init__(self, pins=dict(step=18, direction=23, enable=24)):
+    def __init__(self, pins=default_pins):
         self.pins = pins
         self.pi = pigpio.pi()
 
         for name, pin_num in self.pins.items():
             self.pi.set_mode(pin_num, pigpio.OUTPUT)
 
-    def move(self, revolutions, direction, rev_per_sec=1):
+        self.pi.write(self.pins['reset'], 1) 
+        self.pi.write(self.pins['sleep'], 1) 
+
+
+    def move(self, revolutions, direction, microstep_resolution=8, rev_per_sec=1):
+        
+        ## set microstep pins
+        ms1, ms2, ms3 = microstep_truth_table[
+            power_map.get(microstep_resolution, 3)
+        ]
+        self.pi.write(self.pins['ms1'], ms1) 
+        self.pi.write(self.pins['ms2'], ms2) 
+        self.pi.write(self.pins['ms3'], ms3) 
+
+        print(ms1, ms2, ms3)
 
         rev_per_sec = float(rev_per_sec)
-        microstep_resolution = 8
         steps_per_rev = 200
         
         step_delay = 1 / (steps_per_rev * microstep_resolution * rev_per_sec)
@@ -28,15 +68,24 @@ class StepperDriver():
 
         print(steps, step_delay*1e6)
         for n in range(steps):
-            wf.append(pigpio.pulse(1<<self.pins['step'], 0, 1 * step_delay*1e6))
-            wf.append(pigpio.pulse(0, 1<<self.pins['step'], 1 * step_delay*1e6))
+            wf.append(pigpio.pulse(1<<self.pins['step'], 0, int(step_delay*1e6 / 2)))
+            wf.append(pigpio.pulse(0, 1<<self.pins['step'], int(step_delay*1e6 / 2)))
+
+            if n % 1000 == 0 and n > 0:
+                print(n, len(wf))
+                self.pi.wave_add_generic(wf)
+                wf = []
+                wf.append(pigpio.pulse(0, 0, self.pi.wave_get_micros()))
         
         self.pi.wave_add_generic(wf)
 
         wid = self.pi.wave_create()
         self.pi.wave_send_once(wid)
 
-        time.sleep(0.1)
+        print(self.pi.wave_get_micros() * 1e-6)
+
+        move_time = self.pi.wave_get_micros() * 1e-6
+        time.sleep(move_time + 0.1)
         self.pi.write(self.pins['enable'], 1) ## disable FETs
         
 
@@ -45,6 +94,13 @@ class StepperDriver():
 if __name__ == '__main__':
     stepper_driver = StepperDriver()
 
-    stepper_driver.move(0.5, 0)
-    time.sleep(2)
-    stepper_driver.move(0.5, 1)
+    revs = 2.2
+    stepper_driver.move(revs, 0, microstep_resolution=8, rev_per_sec = 1)
+    time.sleep(0.2)
+    stepper_driver.move(revs, 1, microstep_resolution=8, rev_per_sec = 1)
+        
+    # time.sleep(2)
+    # stepper_driver.move(1, 1)
+    # stepper_driver.move(1, 1)
+    # stepper_driver.move(1, 1)
+    # stepper_driver.move(1, 1)
